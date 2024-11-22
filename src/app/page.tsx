1ba2bfa3c67abd73a45b117a09054acd5c148e9b
Ayush Baral <ayushbaral010@gmail.com>
2024-11-22 20:52:43 +0530

    2.0.5v

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Background } from "@/components/hero-background/background";
 import { Suspense } from 'react'
 import Loader from '@/components/Loader';
 
-export default function MyApp() {
+export default function Home() {
   return (
     <div>
       <Suspense fallback={<Loader />}>
